3587f0909b17c8d80205ac765cfab3a977efbaf1
Emmanuel Adesola <adesola.emmanuel@yahoo.com>
2023-03-09 01:22:34 +0000

    Initial Commit

---
+++ Bop/BopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BopApp.swift
+//  Bop
+//
+//  Created by Emmanuel Adesola on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
